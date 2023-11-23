295e3f6bbd202f1116a0e19e11371aa351604bc9
KimIHwan <lionf2000@naver.com>
2023-11-23 17:49:30 +0900

    기본적인 틀

---
+++ app/login/page.js
@@ -0,0 +1,6 @@
+export default function Login(){
+
+    return(
+        <>로그인창</>
+    )
+}
---
+++ app/main/page.js
@@ -0,0 +1,10 @@
+export default function Main() {
+    return (
+        <div className="main">
+          <h1 className={"title"}>노래방 검색</h1>
+          <h3 className={"title-sub"}>※안전한 놀이터</h3>
+          <p><input type="text" className="search"/></p>
+        </div>
+    )
+  }
+  
