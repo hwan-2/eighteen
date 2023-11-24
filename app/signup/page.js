b81b7f1cf343c31268bd49795c058d8884a27b54
KimIHwan <lionf2000@naver.com>
2023-11-24 23:00:48 +0900

    회원가입 데이터 보내기
    테스트

--- app/signup/page.js
+++ app/signup/page.js
@@ -11,10 +11,13 @@ export default function Signup(){
         <div className={"signup"}>
             <div className={"register"}>
                 <h1>회원가입</h1>
-                <TextField label="이름" name="name" required/>
-                <TextField label="이메일" name="email" required />
-                <TextField label="비밀번호" name="password" type="password" required/>
-                <Button type="submit">가입</Button>
+                <form method="POST" action="/api/auth/signUp">
+                    <TextField label="이름" name="username" required/>
+                    <TextField label="이메일" name="email" required />
+                    <TextField label="비밀번호" name="password" type="password" required/>
+                    <Button type="submit">가입</Button>
+                </form>
+                
             </div>
             
         </div>
