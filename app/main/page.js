fc51ccd027d1cdf155a98effccc6ef65915faae0
KimIHwan <lionf2000@naver.com>
2023-11-27 22:07:19 +0900

    검색창 수정하다 일시정지

--- app/main/page.js
+++ app/main/page.js
@@ -1,10 +1,16 @@
+import { useSession, signIn, signOut } from 'next-auth/react'
+import './main.css'
+
 export default function Main() {
-    return (
-        <div className="main">
-          <h1 className={"title"}>노래방 검색</h1>
-          <h3 className={"title-sub"}>※안전한 놀이터</h3>
-          <p><input type="text" className="search"/></p>
+  return (
+      <div className="mMain">
+        <h1 className={"title"}>노래방 검색</h1>
+        <h3 className={"title-sub"}>※안전한 놀이터</h3>
+        <div className="search">
+          <input type="text" className="searchBar"/>
+          
         </div>
-    )
-  }
+      </div>
+  )
+}
   
