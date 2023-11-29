a6bcb523f738ce3d1aab0ca417c7a6da0f55d742
이승환 <dyrhldnjcl11@gmail.com>
2023-11-29 21:47:34 +0900

    Fix: search api brand필터링 추가

--- pages/api/search/searchSinger.js
+++ pages/api/search/searchSinger.js
@@ -6,10 +6,10 @@ export default async function handler(req, res) {
         try {
             req.body = JSON.parse(req.body)
             const singer = req.body.singer
-            const brand = req.body.brand;
-            const response = await axios.get(`https://api.manana.kr/karaoke/singer/${singer}.json?brand=${brand}`);
-            //json데이터로 반환
-            res.status(200).json(response.data);
+            const response = await axios.get(`https://api.manana.kr/karaoke/singer/${singer}.json`);
+            //response.data를 brand가 tj이거나 kumyoung인 것만 필터링
+            const filteredData = response.data.filter(data => data["brand"] === "tj" || data["brand"] === "kumyoung");
+            res.status(200).json(filteredData);
 
         } catch (error) {
             console.error('API 호출 오류:', error);
