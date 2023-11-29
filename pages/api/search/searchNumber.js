0e1c0d7eb83dff9ad536ff96280a31f13357c11b
이승환 <dyrhldnjcl11@gmail.com>
2023-11-29 21:24:49 +0900

    Fix: api 작동방식 수정

--- pages/api/search/searchNumber.js
+++ pages/api/search/searchNumber.js
@@ -2,15 +2,18 @@ import axios from 'axios';
 
 //번호로 검색하는 api
 export default async function handler(req, res) {
-    const { number, brand } = req.query;
+    if (req.method == 'POST'){
+        try {
+            req.body = JSON.parse(req.body)
+            const number = req.body.singer
+            const brand = req.body.brand;
+            const response = await axios.get(`https://api.manana.kr/karaoke/no/${number}.json?brand=${brand}`);
+            //json데이터로 반환
+            res.status(200).json(response.data);
 
-    try {
-        const response = await axios.get(`https://api.manana.kr/karaoke/no/${number}.json?brand=${brand}`);
-        //json데이터로 반환
-        res.status(200).json(response.data);
-
-    } catch (error) {
-        console.error('API 호출 오류:', error);
-        res.status(500).json({ error: 'API 호출 오류' });
+        } catch (error) {
+            console.error('API 호출 오류:', error);
+            res.status(500).json({ error: 'API 호출 오류' });
+        }
     }
 }
--- pages/api/search/searchTitle.js
+++ pages/api/search/searchTitle.js
@@ -2,15 +2,18 @@ import axios from 'axios';
 
 //제목으로 검색하는 api
 export default async function handler(req, res) {
-    const { title, brand } = req.query;
+    if (req.method == 'POST'){
+        try {
+            req.body = JSON.parse(req.body)
+            const title = req.body.singer
+            const brand = req.body.brand;
+            const response = await axios.get(`https://api.manana.kr/karaoke/song/${title}.json?brand=${brand}`);
+            //json데이터로 반환
+            res.status(200).json(response.data);
 
-    try {
-        const response = await axios.get(`https://api.manana.kr/karaoke/song/${title}.json?brand=${brand}`);
-        //json데이터로 반환
-        res.status(200).json(response.data);
-
-    } catch (error) {
-        console.error('API 호출 오류:', error);
-        res.status(500).json({ error: 'API 호출 오류' });
+        } catch (error) {
+            console.error('API 호출 오류:', error);
+            res.status(500).json({ error: 'API 호출 오류' });
+        }
     }
 }
