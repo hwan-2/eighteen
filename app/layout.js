92923fe37aae8fb870490d99b3a779d10eab1e59
KimIHwan <lionf2000@naver.com>
2023-12-18 23:15:02 +0900

    로그인 세션

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
 import { signIn } from 'next-auth/react';
 import './globals.css'
 import Link from "next/link";
-// import NavBar from '@/component/NavBar';
+
+import NavBar from '@/component/NavBar';
 
 
 export const metadata = {
@@ -10,20 +11,27 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+
   return (
       <html lang="en">
       <body>
-        <div className={"navbar"}>
+
+        <NavBar/>
+        {/* <div className={"navbar"}>
           <Link href={"/"}>Home</Link>
-          {/* 이후 로그인 판별 여부로 수정 */}
-          <Link href={"/login"}>Login</Link>
-        </div>
+          {
+            session ?
+            <Link href={"/mypage"}>MyPage</Link> 
+            :<Link href={"/login"}>Login</Link>
+          }
+        </div> */}
         <div className={"wrap"}>
           {children}
         </div>
         <div className={"footer"}>
           <Link href={"/"}>안녕</Link>
         </div>
+
       </body>
       </html>
   )
--- component/NavBar.js
+++ component/NavBar.js
@@ -1,25 +1,29 @@
-// "use client"
-// import Link from "next/link";
-// import { useSession} from 'next-auth/react'
+import Link from "next/link";
+import { authOptions } from "@/pages/api/auth/[...nextauth].js"
+import { getServerSession } from "next-auth"
 
+export default async function NavBar() {
+    let session = await getServerSession(authOptions)
+    if (session) {
+        console.log(session)
+    }
+    else {
+        console.log("로그인x")
+    }
 
-// export default function NavBar() {
-//     const { data: session } = useSession()
-
-
-//     return (
-//         <nav>
-//             <div className={"navbar"}>
-//                 <Link href={"/"}>Home</Link>
-//                 {/* 이후 로그인 판별 여부로 수정 */}
-//                 {
-//                     !session && <Link href={"/login"}>Login</Link>
-//                 }
-//                 {
-//                     session && <Link href={"/mypage"}>MyPage</Link>
-//                 }
+    return (
+        <nav>
+            <div className={"navbar"}>
+                <Link href={"/"}>Home</Link>
+                {/* 이후 로그인 판별 여부로 수정 */}
+                {
+                    session && session.user ? 
+                    <Link href={"/mypage"}>MyPage</Link>
+                    : 
+                    <Link href={"/login"}>Login</Link>
+                }
             
-//             </div>
-//         </nav>
-//     )
-// }
+            </div>
+        </nav>
+    )
+}
