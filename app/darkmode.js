6a69b44200b321a822cf2e6e91df9cedce3d0fd7
이승환 <dyrhldnjcl11@gmail.com>
2024-01-29 22:49:09 +0900

    Fix: dark mode 오류 수정

--- app/darkmode.js
+++ app/darkmode.js
@@ -1,12 +1,13 @@
 'use client'
 
-import {useEffect} from "react";
+import { useState, useEffect} from "react";
 import { useRouter } from "next/navigation"
 import { MdDarkMode } from "react-icons/md";
 import { FaSun } from "react-icons/fa6";
 
 export default function DarkMode(){
     let router = useRouter()
+    const [isDarkMode, setDarkMode] = useState(false);
 
     useEffect(() => {
         let cookie = ('; '+document.cookie).split(`; mode=`).pop().split(';')[0]
@@ -15,26 +16,20 @@ export default function DarkMode(){
         }
     }, []);
 
-    const isDarkMode = () => {
-        let cookie = ("; " + document.cookie)
-            .split(`; mode=`)
-            .pop()
-            .split(";")[0];
-        return cookie === "dark";
-    };
-
     return(
         <span onClick={()=>{
 
             let cookie = ('; '+document.cookie).split(`; mode=`).pop().split(';')[0]
             if (cookie == 'light') {
+                setDarkMode(true)
                 document.cookie = 'mode=dark; max-age=' + (3600 * 24 * 5)
                 router.refresh()
             } else {
+                setDarkMode(false)
                 document.cookie = 'mode=light; max-age=' + (3600 * 24 * 5)
                 router.refresh()
             }
-        }}>{isDarkMode() ? <MdDarkMode style={{ cursor: 'pointer' }}/> : <FaSun style={{ cursor: 'pointer', color: 'white' }}/>}
+        }}>{isDarkMode ? <MdDarkMode style={{ cursor: 'pointer', color: 'white' }}/> : <FaSun style={{ cursor: 'pointer', color: 'white' }}/>}
         </span>
     )
 }
