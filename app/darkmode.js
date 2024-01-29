b801a013d5a1475da18686ede0eed97976dd33fc
이승환 <dyrhldnjcl11@gmail.com>
2024-01-30 00:34:05 +0900

    Fix: darkmode 오류 수정

--- app/darkmode.js
+++ app/darkmode.js
@@ -7,7 +7,6 @@ import { FaSun } from "react-icons/fa6";
 
 export default function DarkMode(){
     let router = useRouter()
-    const [isDarkMode, setDarkMode] = useState(false);
 
     useEffect(() => {
         let cookie = ('; '+document.cookie).split(`; mode=`).pop().split(';')[0]
@@ -18,18 +17,15 @@ export default function DarkMode(){
 
     return(
         <span onClick={()=>{
-
             let cookie = ('; '+document.cookie).split(`; mode=`).pop().split(';')[0]
             if (cookie == 'light') {
-                setDarkMode(true)
                 document.cookie = 'mode=dark; max-age=' + (3600 * 24 * 5)
                 router.refresh()
             } else {
-                setDarkMode(false)
                 document.cookie = 'mode=light; max-age=' + (3600 * 24 * 5)
                 router.refresh()
             }
-        }}>{isDarkMode ? <MdDarkMode style={{ cursor: 'pointer', color: 'white' }}/> : <FaSun style={{ cursor: 'pointer', color: 'white' }}/>}
+        }}>{('; '+document.cookie).split(`; mode=`).pop().split(';')[0] == 'dark' ? <MdDarkMode style={{ cursor: 'pointer', color: 'white' }}/> : <FaSun style={{ cursor: 'pointer', color: 'white' }}/>}
         </span>
     )
 }
