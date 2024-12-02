276045d05e653fde8bf6f4196bad41c3f79c4e2e
이승환 <dyrhldnjcl11@gmail.com>
2024-12-02 19:39:08 +0900

    Update: 개선을 위한 미들웨어 조건과 시작페이지 변경

--- middleware.js
+++ middleware.js
@@ -6,7 +6,8 @@ export async function middleware(req){
     const session = await getToken({req: req, secret: secret})
 
     //main이나 mypage 들어갈때 로그인 유무 체크
-    if(req.nextUrl.pathname.startsWith('/main') || req.nextUrl.pathname.startsWith('/mypage')) {
+    //req.nextUrl.pathname.startsWith('/main') ||
+    if(req.nextUrl.pathname.startsWith('/mypage')) {
         if (session == null) {
             return NextResponse.redirect(new URL('https://eighteen-three.vercel.app/login'))
         }
