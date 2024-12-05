72821d18ce4e2c3814ab10c92df68c5e3e5fed75
KimIHwan <lionf2000@naver.com>
2024-12-05 17:14:26 +0900

    검색 api 수정

--- pages/api/search/searchSinger.js
+++ pages/api/search/searchSinger.js
@@ -14,20 +14,29 @@ export default async function handler(req, res) {
             //session을 이용한 user가 가지고 있는 마이페이지 데이터를 가져오기 위한 방법
             let session = await getServerSession(req, res, authOptions)
             //session 체크
-            if (!session) {
-                return res.status(400).json("세션 오류발생")
-            }
-            let db = (await connectDB).db('eighteen')
-            let userId = session.user._id
-            let resultUser = await db.collection(`users/${userId}`).find().toArray()
-            const filteredData = resultUser.map(({brand, no, _id}) => ({brand, no, _id}))
-            //user데이터와 검색 데이터를 묶어서 보냄
-            const responseData = {
-                music: response.data,
-                user: filteredData
-            }
 
-            res.status(200).json(responseData);
+            let db = (await connectDB).db('eighteen')
+            if (!session) {
+                const responseData = {
+                    music: response.data,
+                }
+    
+                res.status(200).json(responseData);
+                // return res.status(400).json("세션 오류발생")
+            } else {
+                let userId = session.user._id
+                let resultUser = await db.collection(`users/${userId}`).find().toArray()
+                const filteredData = resultUser.map(({brand, no, _id}) => ({brand, no, _id}))
+                //user데이터와 검색 데이터를 묶어서 보냄
+                const responseData = {
+                    music: response.data,
+                    user: filteredData
+                }
+    
+                res.status(200).json(responseData);
+            }
+            
+            
 
         } catch (error) {
             console.error('API 호출 오류:', error);
