890d620766c1d7fbdfc44dfb2cc71efcc2008dc8
KimIHwan <lionf2000@naver.com>
2026-06-03 20:20:06 +0900

    임시 local mypage

--- proxy.ts
+++ proxy.ts
@@ -8,11 +8,11 @@ export async function proxy(req:NextRequest){
 
     //main이나 mypage 들어갈때 로그인 유무 체크
     //req.nextUrl.pathname.startsWith('/main') ||
-    if(req.nextUrl.pathname.startsWith('/mypage')) {
-        if (session == null) {
-            return NextResponse.redirect(new URL('https://eighteen-three.vercel.app/login'))
-        }
-    }
+    // if(req.nextUrl.pathname.startsWith('/mypage')) {
+    //     if (session == null) {
+    //         return NextResponse.redirect(new URL('https://eighteen-three.vercel.app/login'))
+    //     }
+    // }
     //login이나 signup 들어갈때 로그인 유무 체크
     if(req.nextUrl.pathname.startsWith('/login') || req.nextUrl.pathname.startsWith('/signup') || req.nextUrl.pathname == '/') {
         if (session != null) {
